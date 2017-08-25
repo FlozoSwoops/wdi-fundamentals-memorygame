d4ef532d690d189f8fe67283167cea189dfc02cc
Mark Flores <markflores@Marks-MacBook-Pro.local>
2017-08-25 15:57:22 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,16 @@
+ console.log("Up and running!");
+
+
+/*console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);*/
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("user flipped " + cardsInPlay);
