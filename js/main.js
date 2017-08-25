f71645e572bc5c16d361384f681d9dfa6cd92f7b
Mark Flores <markflores@Marks-MacBook-Pro.local>
2017-08-25 16:32:32 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
- console.log("Up and running!");
-
-
-/*console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);*/
-
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
 
 
 var cardOne = cards[0];
+var cardTwo = cards[2];
 
 cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-console.log("user flipped " + cardsInPlay);
+if (cardsInPlay[0] === cardsInPlay[2]) {
+ alert ("You found a match ");
+} else { alert ("Sorry, try again")}
+
+console.log("user flipped " + cardsInPlay[0]);
+console.log("user flipped " + cardsInPlay[1]);
